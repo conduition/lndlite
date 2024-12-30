f310f38a41a642482a28631148702c9fd6c3caa8
conduition <conduition@proton.me>
2024-12-30 00:08:33 +0000

    add basic LND integration test

---
+++ tests/lnd.rs
@@ -0,0 +1,195 @@
+use hyper_util::client::legacy::connect::Connect;
+use lndlite::{base64::Base64, HttpConnector, HttpsConnector, LndRestClient};
+use serde::{Deserialize, Serialize};
+
+use tempdir::TempDir;
+
+use std::{fs, io, net, process, thread, time::Duration};
+
+const LND_SPINUP_TIME: Duration = Duration::from_millis(100);
+
+pub struct LND<C> {
+    #[allow(dead_code)]
+    tempdir: TempDir,
+    #[allow(dead_code)]
+    child: process::Child,
+    addr: net::SocketAddr,
+    proto: &'static str,
+    connector: C,
+}
+
+fn get_unused_bind_address() -> io::Result<net::SocketAddr> {
+    net::TcpListener::bind("127.0.0.1:0")?.local_addr()
+}
+
+impl LND<HttpsConnector> {
+    pub fn run_regtest() -> io::Result<Self> {
+        let tempdir = TempDir::new("lndlite").expect("error making tempdir");
+        let addr = get_unused_bind_address()?;
+
+        let mut child: process::Child = process::Command::new("lnd")
+            .arg("--bitcoin.regtest")
+            .arg("--bitcoin.node=nochainbackend")
+            .arg(format!("--lnddir={}", tempdir.path().display()))
+            .arg(format!("--restlisten={}", addr))
+            .arg(format!("--rpclisten={}", get_unused_bind_address()?))
+            .stdout(process::Stdio::null())
+            .spawn()?;
+
+        // Give the REST server time to spin up.
+        // TODO: detect automatically when "gRPC proxy started" is logged
+        thread::sleep(LND_SPINUP_TIME);
+
+        let tls_cert_pem = fs::read(tempdir.path().join("tls.cert")).unwrap_or_else(|e| {
+            let _ = child.kill();
+            panic!("failed to read tls.cert from lnd tempdir: {e}");
+        });
+
+        let connector = lndlite::self_signed_https_connector(&tls_cert_pem).unwrap_or_else(|e| {
+            let _ = child.kill();
+            panic!("tls.cert is invalid: {e}");
+        });
+
+        let lnd = LND {
+            tempdir,
+            child,
+            addr,
+            proto: "https",
+            connector,
+        };
+
+        Ok(lnd)
+    }
+}
+
+impl LND<HttpConnector> {
+    pub fn run_regtest() -> io::Result<Self> {
+        let tempdir = TempDir::new("lndlite").expect("error making tempdir");
+        let addr = get_unused_bind_address()?;
+
+        let child: process::Child = process::Command::new("lnd")
+            .arg("--bitcoin.regtest")
+            .arg("--bitcoin.node=nochainbackend")
+            .arg(format!("--lnddir={}", tempdir.path().display()))
+            .arg(format!("--restlisten={}", addr))
+            .arg(format!("--rpclisten={}", get_unused_bind_address()?))
+            .arg("--no-rest-tls")
+            .stdout(process::Stdio::null())
+            .spawn()?;
+
+        // Give the REST server time to spin up.
+        // TODO: detect automatically when "gRPC proxy started" is logged
+        thread::sleep(LND_SPINUP_TIME);
+
+        let connector = HttpConnector::new();
+
+        let lnd = LND {
+            tempdir,
+            child,
+            addr,
+            proto: "http",
+            connector,
+        };
+        Ok(lnd)
+    }
+}
+
+impl<C> LND<C>
+where
+    C: Connect + Clone + Send + Sync + 'static,
+{
+    pub fn unauthed_client(&self) -> LndRestClient<C>
+    where
+        C: hyper_util::client::legacy::connect::Connect + Clone + Send + Sync + 'static,
+    {
+        let mut client =
+            LndRestClient::new_with_connector(self.addr.to_string(), &[], self.connector.clone());
+        if self.proto == "http" {
+            client.unsafe_use_plaintext_http_scheme();
+        }
+        client
+    }
+}
+
+impl<C> std::ops::Drop for LND<C> {
+    fn drop(&mut self) {
+        self.child.kill().expect("failed to kill LND child process");
+    }
+}
+
+#[tokio::test]
+async fn basic_lnd() {
+    let lnd = LND::<HttpsConnector>::run_regtest().expect("failed to run LND");
+    let client = lnd.unauthed_client();
+
+    #[derive(Deserialize)]
+    struct StateUpdateResult {
+        state: String,
+    }
+    #[derive(Deserialize)]
+    struct StateUpdate {
+        result: StateUpdateResult,
+    }
+
+    let mut state_stream = client
+        .get_streamed("/v1/state/subscribe")
+        .await
+        .expect("failed to subscribe");
+    let init_state: StateUpdate = state_stream
+        .next()
+        .await
+        .expect("failed to get state update")
+        .expect("should receive initial state update");
+    assert_eq!(&init_state.result.state, "NON_EXISTING");
+
+    const XPRIV: &'static str = concat!(
+        "tprv8ZgxMBicQKsPdpgo44ctecZXcupSdNCZL5gmLS6FiUzrjqePmp7",
+        "KSSdJcCJ2z2w7aAh1poJBEQcLXi82KzeSg2tBZRvwwUynFrux1NUSuBa",
+    );
+
+    #[derive(Serialize)]
+    struct InitWalletRequest {
+        wallet_password: Base64<String>,
+        extended_master_key: String,
+    }
+    #[derive(Deserialize)]
+    struct InitWalletResponse {
+        admin_macaroon: Base64<Vec<u8>>,
+    }
+
+    let InitWalletResponse {
+        admin_macaroon: Base64(macaroon),
+    } = client
+        .post(
+            "/v1/initwallet",
+            InitWalletRequest {
+                wallet_password: Base64("password123".to_string()),
+                extended_master_key: XPRIV.to_string(),
+            },
+        )
+        .await
+        .expect("failed to initialize LND wallet");
+
+    assert!(!macaroon.is_empty());
+
+    let update: StateUpdate = state_stream
+        .next()
+        .await
+        .expect("failed to get state update")
+        .expect("should receive second state update");
+    assert_eq!(&update.result.state, "UNLOCKED");
+
+    let update: StateUpdate = state_stream
+        .next()
+        .await
+        .expect("failed to get state update")
+        .expect("should receive third state update");
+    assert_eq!(&update.result.state, "RPC_ACTIVE");
+
+    let update: StateUpdate = state_stream
+        .next()
+        .await
+        .expect("failed to get state update")
+        .expect("should receive third state update");
+    assert_eq!(&update.result.state, "SERVER_ACTIVE");
+}
